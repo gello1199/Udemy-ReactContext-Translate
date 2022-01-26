9e5092eab3a6fb170387c0a478d83318bbc6233f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-26 16:35:32 -0500

    Create LanguageContext context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Button extends Component {
     render() {
         return (
-            <button className="ui primary">Submit</button>
+            <button className="ui button primary">Submit</button>
         );
     }
 }
