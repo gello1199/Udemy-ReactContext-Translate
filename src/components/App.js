f38d868188219c710cb725a70f58f4f00ada81aa
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-27 12:31:10 -0500

    Connect LanguageSelector to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from './LanguageSelector';
 
 class App extends Component {
 
-    state = {
-        language: 'english'
-    }
+    // state = {
+    //     language: 'english'
+    // }
 
     // onLanguageChange = language => {
     //     this.setState({ language: language })
     // }
     // refactor below since key and value are the same
 
-    onLanguageChange = language => {
-        this.setState({ language })
-    }
+    // onLanguageChange = language => {
+    //     this.setState({ language })
+    // }
 
     render() {
         return (
             <div className="ui container">
-                <LanguageSelector onLanguageChange={this.onLanguageChange} />
-                
-                <ColorContext.Provider value={"red"}>
-                    <LanguageContext.Provider value={this.state.language}>
-                        <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
+                <LanguageStore>
+                    <LanguageSelector />
+
+                    <ColorContext.Provider value={"red"}>
+                            <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
