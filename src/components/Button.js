310794919efb7ee27f88df884d18d440c932a6a3
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-27 11:21:25 -0500

    Refactor button to access data with Consumers

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,40 @@
+// Accessing data with Consumers
+
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
-    static contextType = LanguageContext;
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen'
+    }
 
     render() {
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
         return (
-            <button className="ui button primary">{text}</button>
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {(value) => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
         );
     }
 }
 
 export default Button;
+
+// using this.contextType
+
+// import React, { Component } from 'react';
+// import LanguageContext from '../contexts/LanguageContext';
+
+// class Button extends Component {
+//     static contextType = LanguageContext;
+
+//     render() {
+//         const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
+//         return (
+//             <button className="ui button primary">{text}</button>
+//         );
+//     }
+// }
+
+// export default Button;
