b56a7dc33b6a3cca644706be85b27c022752be4d
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-27 12:16:20 -0500

    Add Context.Provider to link and pass children components from store

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -2,10 +2,20 @@ import React from "react";
 
 const Context = React.createContext('english');
 
-class LanguageStore extends React.Component {
+export class LanguageStore extends React.Component {
     state = { langage: 'english'}
 
     onLanguageChange = (language) => {
         this.setState({ language })
     }
+
+    render() {
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange }}>
+            {this.props.children}
+            </Context.Provider>
+        )
+    }
 }
+
+export default Context;
